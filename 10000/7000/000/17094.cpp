9b6cdc13d67ac925cb080e47d72fbfac235bb0c9
TLSEHDDBS <sdy4277@naver.com>
2022-09-07 16:22:19 +0900

    220907
    문제 해결

---
+++ 10000/7000/000/17094.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+int main()
+{
+    int s, cnt1 = 0, cnt2 = 0;
+    std::string str;
+    std::cin >> s >> str;
+    
+    for(int i = 0; i < str.length(); i++)
+        str[i] == '2' ? cnt1++ : cnt2++;
+        
+    if(cnt1 == cnt2)
+        std::cout << "yee";
+    else
+        std::cout << (cnt1 > cnt2 ? "2" : "e");
+}
