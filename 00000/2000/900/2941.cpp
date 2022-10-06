cd6f59b14be3903b7c66e307afb471684772aace
TLSEHDDBS <sdy4277@naver.com>
2022-10-06 15:34:44 +0900

    221006
    문제 해결

---
+++ 00000/2000/900/2941.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+int main()
+{
+    int tmp;
+    std::string str, a[8] = { "c=", "c-", "dz=", "d-", "lj", "nj", "s=", "z=" };
+    std::cin >> str;
+    for(int i = 0; i < 8; i++)
+    {
+        while(true)
+        {
+            tmp = str.find(a[i]);
+            if(tmp == std::string::npos)
+                break;
+            str.replace(tmp, a[i].length(), ".");
+        }
+    }
+    std::cout << str.length();
+}
