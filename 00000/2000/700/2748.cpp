8ec6445924028c93b06295f6208ccce9da7d8df8
tlsehddbs <sdy4277@naver.com>
2022-10-22 18:10:52 +0900

    221022
    문제 해결

---
+++ 00000/2000/700/2748.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+long long fibonacci(int n)
+{
+    if(n == 0)
+        return 0;
+    else if(n == 1)
+        return 1;
+    long long fibarr[91] = {0, 1, };
+
+    for(int i = 2; i < n + 1; i++)
+    {
+        fibarr[i] = fibarr[i - 1] + fibarr[i - 2];
+    }
+    return fibarr[n];
+}
+
+int main()
+{
+    int n;
+    std::cin >> n;
+    std::cout << fibonacci(n);
+}
