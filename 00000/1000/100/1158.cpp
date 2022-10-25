bd7b1e48efc602807c92cbe454838deb6db5c169
TLSEHDDBS <sdy4277@naver.com>
2022-10-25 17:29:20 +0900

    221025
    문제 해결

---
+++ 00000/1000/100/1158.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <queue>
+
+int main()
+{
+    std::ios_base::sync_with_stdio(false);
+    std::cout.tie(NULL);
+    
+    int n, k;
+    std::queue<int> q;
+    std::cin >> n >> k;
+    for(int i = 1; i <= n; i++)
+        q.push(i);
+    std::cout << "<";
+    while(!q.empty())
+    {
+        for(int i = 1; i < k; i++)
+        {
+            q.push(q.front());
+            q.pop();
+        }
+        std::cout << q.front();
+        if(q.size() > 1)
+            std::cout << ", ";
+        q.pop();
+    }
+    std::cout << ">";
+}
