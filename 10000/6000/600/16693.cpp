8d4d943b8acf8bea63e43ad41ff67f935a37f1f6
TLSEHDDBS <sdy4277@naver.com>
2022-11-17 12:27:18 +0900

    221117
    문제 해결

--- 10000/6000/600/16693.cpp
+++ 10000/6000/600/16693.cpp
@@ -5,5 +5,5 @@ int main()
 {
     double a1, p1, r1, p2;
     std::cin >> a1 >> p1 >> r1 >> p2;
-    std::cout << (a1 / p1 > (r1 * r1 * 3.14) / p2 ? "Slice of pizza" : "Whole pizza");
+    std::cout << (a1 / p1 > (r1 * r1 * 3.14159265359) / p2 ? "Slice of pizza" : "Whole pizza");
 }
