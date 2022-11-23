73ee198f140df6ba9d56e334ac5a1f6ed1262e0a
TLSEHDDBS <sdy4277@naver.com>
2022-11-23 19:47:57 +0900

    221123
    문제 해결

--- 10000/6000/000/16017.cpp
+++ 10000/6000/000/16017.cpp
@@ -4,8 +4,5 @@ int main()
 {
     int a, b, c, d;
     std::cin >> a >> b >> c >> d;
-    if((a == 8 || a == 9) && b == c && (d == 8 || d == 9))
-        std::cout << "ignore";
-    else
-        std::cout << "answer";
+    std::cout << ((a == 8 || a == 9) && b == c && (d == 8 || d == 9) ? "ignore" : "answer");
 }
