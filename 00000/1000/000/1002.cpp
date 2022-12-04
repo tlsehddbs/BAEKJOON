df9f618b7f7b9dbb862b34472a73082d298da73f
TLSEHDDBS <sdy4277@naver.com>
2022-12-04 16:46:49 +0900

    221204
    문제 해결

---
+++ 00000/1000/000/1002.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <cmath>
+
+int main()
+{
+    int t, x1, y1, x2, y2, r1, r2;
+    double dist, sub;
+    std::cin >> t;
+    for(int i = 0; i < t; i++)
+    {
+        std::cin >> x1 >> y1 >> r1 >> x2 >> y2 >> r2;
+        dist = std::sqrt(std::pow(x2 - x1, 2) + std::pow(y2 - y1, 2));
+        sub = r1 > r2 ? r1 - r2 : r2 - r1;
+
+        if(dist == 0 && r1 == r2)
+            std::cout << "-1\n";
+        else if(r1 + r2 == dist || sub == dist)
+            std::cout << "1\n";
+        else if(sub < dist && dist < (r1 + r2))
+            std::cout << "2\n";
+        else
+            std::cout << "0\n";
+    }
+}
