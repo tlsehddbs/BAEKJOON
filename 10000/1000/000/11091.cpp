c08d17c6a3aa100c08ddcc615b44efcd592ecfd2
TLSEHDDBS <sdy4277@naver.com>
2022-12-06 23:13:12 +0900

    221206
    문제 해결

---
+++ 10000/1000/000/11091.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <algorithm>
+
+int main()
+{
+    int n;
+    std::string str;
+    std::cin >> n;
+    std::cin.ignore();
+    for(int i = 0; i < n; i++)
+    {
+        int tmp, arr[26] = {}, arrcnt = 0;
+        std::getline(std::cin, str);
+        std::transform(str.begin(), str.end(), str.begin(), ::towlower);
+        for(int j = 0; j < str.length(); j++)
+        {
+            tmp = str[j] - 97;
+            arr[tmp]++;
+        }
+        for(int j = 0; j < 26; j++)
+        {
+            if(arr[j] != 0)
+                arrcnt++;
+        }
+        if(arrcnt == 26)
+        {
+            std::cout << "pangram\n";
+        }
+        else
+        {
+            std::cout << "missing ";
+            for(int j = 0; j < 26; j++)
+            {
+                if(arr[j] == 0)
+                    std::cout << char(j + 97);
+            }
+            std::cout << "\n";
+        }
+    }
+}
