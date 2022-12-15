e36c3c10eb973fe0c20ab17b814f2cdf078f560f
TLSEHDDBS <sdy4277@naver.com>
2022-12-15 23:10:48 +0900

    221215
    문제 해결

---
+++ 00000/1000/400/1475.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+int main()
+{
+    int num[10] = { }, max = 0, tmp = 0;
+    std::string n;
+    std::cin >> n;
+    for(int i = 0; i < n.length(); i++)
+        num[n[i] - '0']++;
+    tmp = num[6] + num[9] + 1;
+    for(int i = 0; i < 10; i++)
+    {
+        if(i == 6 || i == 9)
+            max = (tmp / 2 > max ? tmp / 2 : max);
+        else
+            max = (num[i] > max ? num[i] : max);
+    }
+    std::cout << max;
+}
