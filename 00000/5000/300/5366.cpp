e1021983244088aa0c3db2af8b7baf0677a85275
TLSEHDDBS <sdy4277@naver.com>
2023-03-12 16:54:09 +0900

    230312
    문제 해결

---
+++ 00000/5000/300/5366.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <vector>
+#include <unordered_map>
+
+int main()
+{
+    std::ios_base::sync_with_stdio(false);
+    std::cin.tie(NULL);
+    std::cout.tie(NULL);
+
+    int cnt = 0;
+    std::string str;
+    std::vector<std::string> sv;
+    std::unordered_map<std::string, int> m;
+    while(true)
+    {
+        std::getline(std::cin, str);
+        if(str == "0")
+        {
+            for(int i = 0; i < sv.size(); i++)
+            {
+                for(auto a = m.begin(); a != m.end(); a++)
+                {
+                    if(sv[i] == a->first)
+                    {
+                        std::cout << a->first << ": " << a->second << "\n";
+                        cnt += a->second;
+                    }
+                }
+            }
+            std::cout << "Grand Total: " << cnt;
+            return 0;
+        }
+        if(m[str] == 0)
+            sv.push_back(str);
+        m[str]++;
+    }
+}
