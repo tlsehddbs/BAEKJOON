53c9878fcd928a350c8ad0d421684c4fc697d329
TLSEHDDBS <sdy4277@naver.com>
2023-03-16 23:31:02 +0900

    230316
    문제 해결

---
+++ 10000/0000/800/10815.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+int main()
+{
+    std::ios_base::sync_with_stdio(false);
+    std::cin.tie(NULL);
+    std::cout.tie(NULL);
+
+    int n, m, tmp;
+    std::vector<int> v;
+    std::cin >> n;
+    for(int i = 0; i < n; i++)
+    {
+        std::cin >> tmp;
+        v.push_back(tmp);
+    }
+    std::sort(v.begin(), v.end());
+    std::cin >> m;
+    for(int i = 0; i < m; i++)
+    {
+        std::cin >> tmp;
+        std::cout << (std::binary_search(v.begin(), v.end(), tmp) ? 1 : 0) << " ";
+    }
+}
