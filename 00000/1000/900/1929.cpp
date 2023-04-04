63cd2fbff3f734f86ea6774ca6343fcea96bfe9b
tlsehddbs <sdy4277@naver.com>
2023-04-04 17:51:47 +0900

    Update 1929.cpp
    arr 자료형 변환

--- 00000/1000/900/1929.cpp
+++ 00000/1000/900/1929.cpp
@@ -5,14 +5,15 @@ int main()
     std::ios_base::sync_with_stdio(false);
     std::cout.tie(NULL);
 
-    int m, n, arr[1000000] = {};
+    int m, n;
+    bool arr[1000000] = {};
     std::cin >> m >> n;
     m = m < 2 ? 2 : m;
     for(int i = 2; i <= n; i++)
         for(int j = 2 * i; j <= n; j += i)
-            if(arr[j] == 0)
+            if(!arr[j])
                 arr[j] = 1;
     for(int i = m; i <= n; i++)
-        if(arr[i] == 0)
+        if(!arr[i])
             std::cout << i << "\n";
 }
